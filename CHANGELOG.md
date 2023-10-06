8173e1fb0a4930f74b828a3ee999b062cecc16c9
Bruno Gritti <glbrunogl@gmail.com>
2023-10-06 14:22:04 -0300

    event resource and EventRequest

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,10 +3,11 @@
 All notable changes to this project will be documented in this file ordered by newest first.
 
 
-## [1.0.1] - 2023-10-04 - Bruno Gritti
+## [1.0.1] - 2023-10-06 - Bruno Gritti
  
 #### Added
  - Factories and command to generate test data
+ - Event Resource routes and EventRequest
 #### Changed
 #### Fixed
 
--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EventRequest;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
-    //
+    public function index()
+    {
+        try {
+            $events = Event::all();
+
+            return response()->json($events);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => "An error ocurred",
+            ], 422);
+        }
+    }
+
+    public function store(EventRequest $eventRequest)
+    {
+        try {
+            $events = Event::all();
+
+            return response()->json($events);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => $e->getMessage(),
+                'message' => "An error ocurred",
+            ], 422);
+        }
+    }
 }
